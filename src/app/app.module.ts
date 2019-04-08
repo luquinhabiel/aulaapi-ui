06e86133a04c6a12956f636df7a1b18a944f9c28
Aluno <aluno@eteclencois.com.br>
2019-04-08 11:42:50 -0300

    Criar menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriasModule } from './categorias/categorias.module';
+import { CoreModule } from './core/core.module';
 
-import {InputTextModule} from 'primeng/inputtext';
-import {ButtonModule} from 'primeng/button';
-import {TableModule} from 'primeng/table';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    InputTextModule,
-    ButtonModule,
-    TableModule
+    CoreModule, 
+    CategoriasModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/categorias/categorias-pesquisa/categorias-pesquisa.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categorias-pesquisa',
+  templateUrl: './categorias-pesquisa.component.html',
+  styleUrls: ['./categorias-pesquisa.component.css']
+})
+export class CategoriasPesquisaComponent implements OnInit {
+
+  categorias = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
